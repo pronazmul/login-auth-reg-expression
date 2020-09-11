181504413daa67599705f0da562dbfe92488a5ee
Nazmul Huda <mhnazmul48@gmail.com>
2020-09-11 13:44:16 +0600

    Regular Expression test & Add Email & Password on Firebase Done

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig/firebaseConfig';
+
+firebase.initializeApp(firebaseConfig)
+
+const LoginForm = () => {
+    const [user, setUser] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        success:false,
+        error:''
+    })
+    const handleSubmit = (event) => {
+
+        console.log(user.email, user.password)
+        if (user.email && user.password) {
+
+            firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+                .then(data => {
+                    let newUser = { ...user }
+                    newUser.success = true
+                    newUser.error =''
+                    setUser(newUser)
+                })
+                .catch(error =>{
+                    let newUser = { ...user }
+                    newUser.success = false
+                    newUser.error =error.message
+                    setUser(newUser)
+                });
+        }
+
+        event.preventDefault()
+    }
+    const handleBlur = (event) => {
+        let name = event.target.name
+        let value = event.target.value
+
+        let checkedData
+
+        if (name === 'userName') {
+            //Name Should be UpperCase Or LowerCase && length should be 5 to 8 
+            let namePattern = /^([a-zA-Z]){5,8}$/;
+            checkedData = namePattern.test(value)
+        }
+
+        if (name === 'email') {
+            //Name Should be UpperCase Or LowerCase && length should be 5 to 8 
+            let emailPattern = /\S+@\S+\.\S+/;
+            checkedData = emailPattern.test(value)
+        }
+
+        if (name === 'password') {
+            //Should be UpperCase, LowerCase, Number && length should be 6 to 12
+            let pwdPattern = /^(?=.*[0-9])(?=(?:[^A-Z]*[A-Z]){1})(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,12}$/;
+            checkedData = pwdPattern.test(value)
+        }
+        console.log(checkedData)
+        if (checkedData) {
+            let newUser = { ...user }
+            newUser[name] = value
+            setUser(newUser)
+        }
+
+    }
+    return (
+        <div className='container'>
+            <h1 className='jumbotron text-center text-success display-4 py-4'>Simple Login With Regular Expression</h1>
+            {user.success && <h4 className='text-center text-success'>User Added Successfully</h4>}
+            {user.error && <h4 className='text-center text-danger'>{user.error}</h4>}
+            <form>
+                <div className='form-group col-md-4 mx-auto'>
+                    <input onBlur={handleBlur} name='userName' type='text' className='form-control ' placeholder='Enter Username'></input>
+                    <input onBlur={handleBlur} name='email' type='email' className='form-control mt-2' placeholder='Enter Email'></input>
+                    <input onBlur={handleBlur} name='password' type='password' className='form-control mt-2' placeholder='Enter Password'></input>
+                    <input onClick={handleSubmit} className=' btn btn-success rounded mt-3 btn-lg' type='submit' value='Sign Up'></input>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
---
+++ src/Components/LoginForm/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJFsXTvtGJRoC-wmrIJ-8Ukge14oHGOSQ",
+    authDomain: "first-firebase-project-36d87.firebaseapp.com",
+    databaseURL: "https://first-firebase-project-36d87.firebaseio.com",
+    projectId: "first-firebase-project-36d87",
+    storageBucket: "first-firebase-project-36d87.appspot.com",
+    messagingSenderId: "670551618814",
+    appId: "1:670551618814:web:47034df450c9a9ce3f21af"
+  };
+
+  export default firebaseConfig
